a2e22d3e9b06f74967a1b7717b9f934f3f585195
Ju-MINJAE <juminjae98@gmail.com>
2024-10-01 10:30:15 +0900

    JavaScript_calculator_4

--- JavaScript/calculator/script.js
+++ JavaScript/calculator/script.js
@@ -2,23 +2,71 @@ const p = document.querySelector('p');
 const buttons = document.querySelectorAll('.button');
 
 let curNum = '0';
+let firstOperand = null;
+let secondOperand = null;
+let operator = null;
 
 buttons.forEach((btn) => {
   btn.addEventListener('click', (e) => {
-    console.log(e.target.textContent);
+    const clickedNum = e.target.textContent;
+    // console.log(`Clicked: ${clickedNum}`);
 
     if (btn.classList.contains('number')) {
-      const clickedNum = e.target.textContent;
-
       if (curNum === '0') curNum = clickedNum;
       else curNum += clickedNum;
     }
 
-    if (btn.classList.contains('dot')) {
+    if (clickedNum === '.') {
       if (!curNum.includes('.')) curNum += '.';
     }
 
-    if (btn.classList.contains('clear')) curNum = '0';
+    if (clickedNum === 'C') {
+      curNum = '0';
+      firstOperand = null;
+      secondOperand = null;
+      operator = null;
+      p.textContent = curNum;
+      return;
+    }
+
     p.textContent = curNum;
+
+    if (btn.classList.contains('operator')) {
+      firstOperand = curNum;
+      console.log(`firstOperand: ${firstOperand}`);
+
+      curNum = '0';
+      operator = clickedNum;
+      console.log(`First Operand: ${firstOperand}, Operator: ${operator}`);
+    }
+
+    if (clickedNum === '=') {
+      if (operator && firstOperand !== null) {
+        secondOperand = curNum;
+        const result = calculate(firstOperand, operator, secondOperand);
+
+        p.textContent = result;
+        firstOperand = result;
+        secondOperand = null;
+        operator = null;
+        curNum = result;
+      }
+    }
   });
 });
+
+function calculate(num1, op, num2) {
+  num1 = Number(num1);
+  num2 = Number(num2);
+
+  switch (op) {
+    case '+':
+      return String(num1 + num2);
+    case '-':
+      return String(num1 - num2);
+    case 'X':
+      return String(num1 * num2);
+    case '/':
+      return String(num1 / num2);
+  }
+}
