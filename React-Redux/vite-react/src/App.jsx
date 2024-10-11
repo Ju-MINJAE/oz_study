a5f76b52fa8a717277f5bc64c9a55cd750e824aa
MinJae <juminjae98@gmail.com>
2024-10-11 11:02:06 +0900

    ✅ Modify App.jsx

--- React-Redux/vite-react/src/App.jsx
+++ React-Redux/vite-react/src/App.jsx
@@ -1,35 +1,98 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, Component } from 'react';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [counter, setCounter] = useState(0);
+  const [inputValue, setInputValue] = useState(0);
+
+  const incrementCounter = () => {
+    setCounter(counter + 1);
+  };
+  const decrementCounter = () => {
+    setCounter(counter - 1);
+  };
+  const setCounterNumber = () => {
+    setCounter(inputValue);
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Count counter={counter} />
+      <PlusButton incrementCounter={incrementCounter} />
+      <MinusButton decrementCounter={decrementCounter} />
+      <CounterInput
+        inputValue={inputValue}
+        setInputValue={setInputValue}
+        setCounterNumber={setCounterNumber}
+      />
     </>
-  )
+  );
 }
 
-export default App
+function CounterInput({ inputValue, setInputValue, setCounterNumber }) {
+  return (
+    <>
+      <input
+        type="number"
+        value={inputValue}
+        onChange={(e) => {
+          setInputValue(e.target.value);
+        }}
+      />
+      <button onClick={setCounterNumber}>입력</button>
+    </>
+  );
+}
+
+function PlusButton({ incrementCounter }) {
+  return <button onClick={incrementCounter}>+</button>;
+}
+function MinusButton({ decrementCounter }) {
+  return <button onClick={decrementCounter}>-</button>;
+}
+
+function Count({ counter }) {
+  return <div>counter : {counter}</div>;
+}
+
+// class App extends Component {
+//   state = { counter: 0 };
+
+//   increamentCounter = () => {
+//     this.setState({ counter: this.state.counter + 1 });
+//   };
+//   decrementCounter = () => {
+//     this.setState({ counter: this.state.counter - 1 });
+//   };
+
+//   render() {
+//     return (
+//       <>
+//         <Count counter={this.state.counter} />
+//         <PlusButton increamentCounter={this.increamentCounter} />
+//         <MinusButton decrementCounter={this.decrementCounter} />
+//       </>
+//     );
+//   }
+// }
+
+// class PlusButton extends Component {
+//   render() {
+//     return <button onClick={this.props.increamentCounter}>+</button>;
+//   }
+// }
+
+// class MinusButton extends Component {
+//   render() {
+//     return <button onClick={this.props.decrementCounter}>-</button>;
+//   }
+// }
+
+// class Count extends Component {
+//   render() {
+//     console.log(this.props);
+//     return <div>counter :{this.props.counter}</div>;
+//   }
+// }
+
+export default App;
